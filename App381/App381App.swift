797b882de171ae31f8895ce1e42a0790f21760c4
IGOLIVIT <131347905+IGOLIVIT@users.noreply.github.com>
2024-03-09 12:53:40 +0300

    Initial Commit

---
+++ App381/App381App.swift
@@ -0,0 +1,17 @@
+//
+//  App381App.swift
+//  App381
+//
+//  Created by IGOR on 09/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct App381App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
